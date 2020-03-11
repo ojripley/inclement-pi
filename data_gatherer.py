82e6566c9b31a3b183aba30149b843a7bf60f3fe
ojripley <ojripley@gmail.com>
2020-03-11 19:57:55 -0400

    add basic sensing. Only prints to console for time being

---
+++ data_gatherer.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+from sense_hat import SenseHat
+import time
+import sys
+
+DATA_READ_INTERVAL = 60 # in seconds
+
+sense = SenseHat()
+sense.clear() # clears the LED matrix
+
+try:
+  while True:
+    temp = sense.get_temperature()
+    humidity = sense.get_humidity()
+    pressure = sense.get_pressure()
+    
+    temp = round(temp, 1)
+    humidity = round(humidity, 1)
+    pressure = round(pressure, 1)
+
+    print("\n\n-- Weather Readout --")
+    print("Temperature:  ", temp, "°C")
+    print("Humidity:     ", humidity + "%")
+    print("Pressure:     ", pressure, "idk what unit this readout is going to come in")
+
+    time.sleep(DATA_READ_INTERVAL)
+
+except KeyboardInterrupt:
+  pass
