17f86747a055c929e8c9b7d76b7008274e138fa5
ojripley <ojripley19@gmail.com>
2020-03-12 12:30:33 -0400

    fix regular expression parsing

--- network_monitor.py
+++ network_monitor.py
@@ -7,13 +7,14 @@ import time
 speedtest_response = subprocess.Popen('/usr/local/bin/speedtest-cli --simple', shell = True, stdout=subprocess.PIPE).stdout.read().decode('utf-8')
 
 # parse response for the data we want
-ping = re.findall('Ping:\s(.*?)\s', speedtest_response, re.MULTILINE)
-download = re.findall('Download:\s(.*?)\s', speedtest_response, re.MULTILINE)
-upload = re.findall('Upload:\s(.*?)\s', speedtest_response, re.MULTILINE)
+ping = re.findall(r'Ping:\s(.*?)\s', speedtest_response, re.MULTILINE)
+download = re.findall(r'Download:\s(.*?)\s', speedtest_response, re.MULTILINE)
+upload = re.findall(r'Upload:\s(.*?)\s', speedtest_response, re.MULTILINE)
 
-ping=ping[0].replace(',', '.')
-download=download[0].replace(',', '.')
-upload=upload[0].replace(',', '.')
+if (ping.__len__ > 0 and download.__len__ and upload.__len__ > 0):
+  ping=ping[0].replace(',', '.')
+  download=download[0].replace(',', '.')
+  upload=upload[0].replace(',', '.')
 
 try:
   file = open('/home/pi/speedtest/speedtest.csv', 'a+')
