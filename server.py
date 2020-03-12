5098d408bbee57534d8eaf42c5b40ca35c6a7712
ojripley <ojripley19@gmail.com>
2020-03-12 11:59:34 -0400

    basic structure for async data reading across multiple modules

--- server.py
+++ server.py
@@ -5,23 +5,31 @@ import asyncio
 
 # climate_sensor = Sensor()
 
-SENSE_READ_INTERVAL = 6  # in seconds
-NETWORK_READ_INTERVAL = 10 # may shorten this, depends how resource intensive speedtest-cli is
+SENSE_READ_INTERVAL = 10  # in seconds
+NETWORK_READ_INTERVAL = 5 # may shorten this, depends how resource intensive speedtest-cli is
 
-async def forward_climate_data(loop):
+async def forward_climate_data():
+  while True:
+    # climate_conditions = climate_sensor.read_data()
+    # print(climate_conditions)
+    print('getting climate data...')
 
-  # climate_conditions = climate_sensor.read_data()
-  # print(climate_conditions)
-  print('getting climate data...')
-
-  # todo: write a websocket protocol to send this to a frontend
-  await asyncio.sleep(SENSE_READ_INTERVAL)
+    # todo: write a websocket protocol to send this to a frontend
+    await asyncio.sleep(SENSE_READ_INTERVAL)
 
 async def forward_network_data():
-  # network_data = 
-  print('getting network data...')
-  await asyncio.sleep(NETWORK_READ_INTERVAL)
+  while True:
+    # network_data = 
+    print('getting network data...')
+    await asyncio.sleep(NETWORK_READ_INTERVAL)
+
 
 loop = asyncio.get_event_loop()
-loop.run_unti(forward_climate_data(loop))
-# loop.run_forever(forward_network_data)
+try:
+  asyncio.ensure_future(forward_climate_data())
+  asyncio.ensure_future(forward_network_data())
+  loop.run_forever()
+
+# break loop on ctrl+c
+except KeyboardInterrupt:
+  pass
