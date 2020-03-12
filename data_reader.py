c0dd866408b4ad49e9c3913e830bbd3c1892f183
ojripley <ojripley19@gmail.com>
2020-03-12 15:42:00 -0400

    store up to date data in a set. Server will be seperate file, and push data from that set

--- data_reader.py
+++ data_reader.py
@@ -1,11 +1,8 @@
 import time
 import asyncio
-import websockets
 # from sense_data_gatherer import Sensor
 from network_monitor import NetworkMonitor
 
-HOST = 'localhost'
-PORT = 8080
 
 # in seconds
 SENSE_READ_INTERVAL = 60  
@@ -14,27 +11,32 @@ NETWORK_READ_INTERVAL = 50
 # climate_sensor = Sensor()
 network_monitor = NetworkMonitor()
 
-async def forward_climate_data():
+# will hold the most up-to-date data from each source
+current_data = dict()
+
+async def get_climate_data():
   while True:
     # climate_conditions = climate_sensor.read_data()
     # print(climate_conditions)
     print('getting climate data...')
+    current_data['climate_data'] = 'climate data'
 
-    # todo: write a websocket protocol to send this to a frontend
     await asyncio.sleep(SENSE_READ_INTERVAL)
 
-async def forward_network_data():
+async def get_network_data():
   while True:
     print('getting network data...')
     network_data = network_monitor.assess_network()
-    # await websocket.send(network_data)
-    print(network_data)
+
+    current_data['network_data'] = network_data
+    print(current_data)
+    
     await asyncio.sleep(NETWORK_READ_INTERVAL)
 
 loop = asyncio.get_event_loop()
 try:
-  asyncio.ensure_future(forward_climate_data())
-  asyncio.ensure_future(forward_network_data())
+  asyncio.ensure_future(get_climate_data())
+  asyncio.ensure_future(get_network_data())
   loop.run_forever()
 
 # break loop on ctrl+c
