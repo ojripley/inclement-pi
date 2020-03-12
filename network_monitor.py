67421db205efbecee6e3a427876eaefc6bd6c968
ojripley <ojripley19@gmail.com>
2020-03-12 12:57:32 -0400

    adjust file paths

--- network_monitor.py
+++ network_monitor.py
@@ -4,22 +4,26 @@ import subprocess
 import time
 
 # run a subprocess and call speedtest-cli. Log its output to the shell and read it
-speedtest_response = subprocess.Popen('/usr/local/bin/speedtest-cli --simple', shell = True, stdout=subprocess.PIPE).stdout.read().decode('utf-8')
+speedtest_response = subprocess.Popen('/usr/bin/speedtest-cli --simple', shell = True, stdout=subprocess.PIPE).stdout.read().decode('utf-8')
 
-# parse response for the data we want
+# parse response for the desired data
 ping = re.findall(r'Ping:\s(.*?)\s', speedtest_response, re.MULTILINE)
 download = re.findall(r'Download:\s(.*?)\s', speedtest_response, re.MULTILINE)
 upload = re.findall(r'Upload:\s(.*?)\s', speedtest_response, re.MULTILINE)
 
-if (ping.__len__ > 0 and download.__len__ and upload.__len__ > 0):
-  ping=ping[0].replace(',', '.')
-  download=download[0].replace(',', '.')
-  upload=upload[0].replace(',', '.')
+print(ping)
+print(download)
+print(upload)
+
+# not sure this is necessary
+ping = ping[0].replace(',', '.')
+download = download[0].replace(',', '.')
+upload = upload[0].replace(',', '.')
 
 try:
   file = open('/home/pi/speedtest/speedtest.csv', 'a+')
   if (os.stat('/home/pi/speedtest/speedtest.csv')).st_size == 0:
-    f.write('Date, Time, Ping (ms), Download (Mb/s), Upload (Mb/s)\r\n')
+    file.write('Date, Time, Ping (ms), Download (Mb/s), Upload (Mb/s)\r\n')
 except:
   pass
 
