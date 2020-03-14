f8b504c084356438e3f05e6e080c87947eaa1703
ojripley <ojripley19@gmail.com>
2020-03-13 23:59:00 -0400

    fix spacing/indenting

--- data_reading/sense_test.py
+++ data_reading/sense_test.py
@@ -2,10 +2,13 @@ import time
 import sense
 
 try:
-	while True:
+  while True:
 
-		climate_conditions = sense.read_data(True)
-    sense.update_display_matrix(climate_conditions)
-		time.sleep(1)
+    climate_data = sense.read_data(True)
+    
+    sense.update_display_matrix(climate_data)
+
+    time.sleep(1)
+
 except KeyboardInterrupt:
 		pass
