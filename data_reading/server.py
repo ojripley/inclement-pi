76d5465a1c1c846ac6b1c5a3087f483f51e335a2
ojripley <ojripley19@gmail.com>
2020-03-14 14:10:06 -0400

    update server to send data on loop

--- data_reading/server.py
+++ data_reading/server.py
@@ -2,17 +2,16 @@ import time
 import asyncio
 import websockets
 import json as json
-# from sense_data_gatherer import Sensor
+import sense
 from network_monitor import NetworkMonitor
 
-HOST = 'localhost'
+HOST = '127.0.0.1'
 PORT = 8080
 
 # in seconds
 SENSE_READ_INTERVAL = 1  
 NETWORK_READ_INTERVAL = 10
 
-# climate_sensor = Sensor()
 network_monitor = NetworkMonitor()
 
 # holds the most up-to-date data from each source
@@ -24,25 +23,6 @@ users = set()
 async def get_current_data():
   return current_data
 
-async def get_climate_data():
-  while True:
-    # climate_conditions = climate_sensor.read_data()
-    # print(climate_conditions)
-    print('getting climate data...')
-    current_data['climate_data'] = 'climate data'
-
-    await asyncio.sleep(SENSE_READ_INTERVAL)
-
-async def get_network_data():
-  while True:
-    print('getting network data...')
-    network_data = network_monitor.assess_network()
-
-    current_data['network_data'] = network_data
-    print(current_data)
-
-    await asyncio.sleep(NETWORK_READ_INTERVAL)
-
 async def register(socket):
   users.add(socket)
 
@@ -52,8 +32,13 @@ async def unregister(socket):
 async def broadcast_data():
   print('broadcast is being run, sending to:')
   print(users)
+
+  climate_data = sense.read_data()
+
   if users: # protect against empty set
-    await asyncio.wait([user.send(json.dumps(current_data)) for user in users])
+    await asyncio.wait([user.send(json.dumps(climate_data)) for user in users])
+
+  time.sleep(2)
 
 async def server(socket, path):
   await register(socket)
@@ -69,8 +54,6 @@ start_server = websockets.serve(server, HOST, PORT)
 loop = asyncio.get_event_loop()
 try:
   loop.run_until_complete(start_server)
-  asyncio.ensure_future(get_climate_data())
-  asyncio.ensure_future(get_network_data())
   loop.run_forever()
 
 # break loop on ctrl+c
