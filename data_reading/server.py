a688a16d4d7901a796ef2326d7568c88777f9319
ojripley <ojripley19@gmail.com>
2020-03-15 14:36:12 -0400

    experimental Sanic server

--- data_reading/server.py
+++ data_reading/server.py
@@ -1,85 +1,53 @@
-import time
+#!/usr/bin/env python
+
 import asyncio
-import websockets
-import json as json
-from sense import Sensor
-from network_monitor import NetworkMonitor
+import uvloop
+import json
 
-HOST = '0.0.0.0'
-PORT = 8080
+from sanic import Sanic
+from sanic import response
+from sanic.response import file
+from sanic.websocket import ConnectionClosed
 
-# in seconds
-SENSE_READ_INTERVAL = 1  
-NETWORK_READ_INTERVAL = 10
+app = Sanic(__name__)
 
-sensor = Sensor()
-network_monitor = NetworkMonitor()
-
-# holds the most up-to-date data from each source
-current_data = dict()
-
-# holds all active client sockets
-users = set()
-
-async def get_current_data():
-  return current_data
-
-async def get_climate_data():
-  print('preparing to read data!')
-  current_data['climate_data'] = sensor.read_data()
-  time_of_reading = time.ctime(time.time())
-  current_data['timestamp'] = time_of_reading
-  print('just got data... im tired, going to sleep now') 
-
-  if (users):
-    print('sending to ')
-    print(users)
-    await asyncio.wait([user.send(json.dumps(current_data)) for user in users])
-
-  time.sleep(1)
-  print('quick nap is over!')
+app.ws_clients = set()
+asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
 
 
-async def register(socket):
-  print('registering' + str(socket))
-  users.add(socket)
-
-async def unregister(socket):
-  users.remove(socket)
-
-async def broadcast_data(socket):
-  print('broadcast is being run, sending to:')
-  print(socket)
-
-  while True:
-    # await socket.send(json.dumps(current_data))
-    # current_data = await get_current_data()
-    if (users):
-      await asyncio.wait([user.send(json.dumps(current_data)) for user in users])
-      print('just sent data: ')
-      print(current_data)
+# @app.route('/')
+# async def index(request):
+#     return await file('ws.html')
 
 
+# @app.route('/post', methods=["POST"])
+# async def message_inbound(request):
+#     payload = request.body.decode("utf-8")
+#     await broadcast(payload)
+#     return response.json({"status": "OK"})
 
-async def server(socket, path):
-  await register(socket)
 
-  # try:
-    # print('preparing to broadcast')
-    # await broadcast_data(socket)
-  # finally:
-    # await unregister(socket)
+async def broadcast(message):
+    broadcasts = [ws.send(message) for ws in app.ws_clients]
+    for result in asyncio.as_completed(broadcasts):
+        try:
+            await result
+        except ConnectionClosed:
+            print("ConnectionClosed")
+        except Exception as ex:
+            template = "An exception of type {0} occurred. Arguments:\n{1!r}"
+            message = template.format(type(ex).__name__, ex.args)
+            print(message)
 
-start_server = websockets.serve(server, HOST, PORT)
 
-loop = asyncio.get_event_loop()
-try:
-  loop.run_until_complete(start_server)
-  asyncio.ensure_future(get_climate_data())
-  print('server is listening at ' + str(HOST) + ':' +str(PORT))
-  loop.run_forever()
+@app.websocket("/")
+async def websocket(request, ws):
+    app.ws_clients.add(ws)
+    await ws.send(json.dumps("hello from server!"))
+    print(f'{len(app.ws_clients)} clients')
+    while True:
+        data = await ws.recv()
+        print('Received: ' + data)
 
-# break loop on ctrl+c
-except KeyboardInterrupt:
-  sensor.clear()
-  pass
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080, workers=1, debug=False)
