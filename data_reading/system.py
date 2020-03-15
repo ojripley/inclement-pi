a1d3823f0ca3b42eb7b127a22ef8cf5dd331e52e
ojripley <ojripley19@gmail.com>
2020-03-15 17:04:50 -0400

    fix cpu temperature reading

--- data_reading/system.py
+++ data_reading/system.py
@@ -7,7 +7,7 @@ def get_system_data():
   system_data = dict()
 
   system_data['cpuPercent'] = psutil.cpu_percent()
-  system_data['cpuTemperature'] = CPUTemperature()
+  system_data['cpuTemperature'] = CPUTemperature().temperature
   system_data['memoryPercent'] = psutil.virtual_memory()
 
   return system_data
