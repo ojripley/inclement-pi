4926692e951d4329a42a02b6ae5444638c8e7e5d
ojripley <ojripley19@gmail.com>
2020-03-16 20:54:10 -0400

    can i instantiate this close error

--- data_reading/command_server.py
+++ data_reading/command_server.py
@@ -48,7 +48,8 @@ async def broadcast(message):
     except ConnectionClosed:
       print("ConnectionClosed")
       print(ConnectionClosed)
-
+      print cc = ConnectionClosed()
+      print cc
     except Exception as ex:
       template = "An exception of type {0} occurred. Arguments:\n{1!r}"
       message = template.format(type(ex).__name__, ex.args)
