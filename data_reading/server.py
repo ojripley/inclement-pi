e2f08f2af4f511fad4b2a282327f37a34617bc74
ojripley <ojripley19@gmail.com>
2020-03-16 23:18:50 -0400

    experimental remove dead connections in server.py

--- data_reading/server.py
+++ data_reading/server.py
@@ -66,8 +66,15 @@ async def broadcast(message):
       sensor.clear()
       pass
 
-
-
+def cull_dead_connections():
+  for (ws in app.ws_clients):
+    try:
+      await ws.send('ping')
+    except ConnectionClosed:
+      clients_to_remove.add(ws)
+  for client in clients_to_remove:
+    app.ws_clients.remove(client)
+  clients_to_remove.clear()
 
 @app.websocket("/")
 async def websocket(request, ws):
@@ -83,6 +90,7 @@ async def websocket(request, ws):
       data['systemData'] = get_system_data()
       data['timestamp'] = time_of_reading
       await broadcast(json.dumps(data))
+      cull_dead_connections()
       time.sleep(5)
     except KeyboardInterrupt:
       sensor.clear()
