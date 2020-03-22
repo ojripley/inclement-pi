4cd2a8b2db9184fd5808105ddbbe5f3971636e16
ojripley <ojripley19@gmail.com>
2020-03-22 15:14:08 -0400

    fix rounding for minute display

--- client/src/components/CameraWidget.js
+++ client/src/components/CameraWidget.js
@@ -19,7 +19,7 @@ export default function CameraWidget(props) {
     <div className="widget">
       <header className={'widget-header'}>Camera View</header>
       <div className={'camera-control-div'}>
-        <p className={'widget-data-text'} >Last Updated: {props.imageLastUpdated > 60 ? props.imageLastUpdated / 60 : props.imageLastUpdated}{props.imageLastUpdated > 60 ? 'm' : 's'} ago </p>
+        <p className={'widget-data-text'} >Last Updated: {props.imageLastUpdated > 60 ? Math.round(props.imageLastUpdated / 60) : props.imageLastUpdated}{props.imageLastUpdated > 60 ? 'm' : 's'} ago </p>
         <button className={'camera-button'} onClick={requestImage}>Update View</button>
       </div>
       {props.image ? <img className={'camera-image'} src={'data:image/jpg;base64,' + props.image}></img> : 
