3dab8dde619b1b34b7c1764df8aaa3c972edc59d
ojripley <ojripley19@gmail.com>
2020-03-30 14:43:39 -0400

    placeholder buttons for toggling weather graph

--- client/src/components/WeatherWidget.js
+++ client/src/components/WeatherWidget.js
@@ -27,7 +27,14 @@ export default function WeatherWidget(props) {
           <p className={'widget-data-text'} >Pressure: {pressure} mBar</p>
           <p className={'widget-data-text'} >Last Updated: {props.lastUpdated}</p>
         </div>
-        <AreaChart id='weather-chart' colors={["#ffff00", "#000000"]} width="50%" height='50%' data={props.climateData.hourly_averages}></AreaChart>
+        <div className={'weather-chart'}>
+          <AreaChart id='graph' colors={["#ffff00", "#000000"]} height={'100%'} data={props.climateData.hourly_averages}></AreaChart>
+          <div className={'button-container'}>
+            <button>Temp</button>
+            <button>Humidity</button>
+            <button>Pressure</button>
+          </div>
+        </div>
       </div>
     </div>
   );
